86a62624730413886c6f860829a4153216445c41
andy <andy.liujin@foxmail.com>
2020-01-01 07:27:33 +0000

    静态文件浏览器缓存问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
+/*
+    在public/app.css中改成: <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    version() 配合 mix() 使用，编译后会变成:<link rel="stylesheet" href="/css/app.css?id=a18bcbec68adfba91268">
+    以此来解决：静态文件浏览器缓存问题
+*/
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css').version();
