d8c9f8f7ae99b0eaa16c1ae51cebacfc15f63d1d
andy <andy.liujin@foxmail.com>
2020-01-04 04:43:25 +0000

    微博数据模型

---
+++ database/migrations/2020_01_04_040221_create_statuses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('status', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('content');
+            $table->integer('user_id')->index(); // 需要根据 user_id 查询用户发布的微博
+            $table->index(['created_at']); // 因为微博会按时间倒序排序输出，所以加索引
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('status');
+    }
+}
